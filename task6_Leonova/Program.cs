9b8c856f905b9d238d7c78f28b8a84d2a89cefcc
marina.leonova <leona_21@mail.ru>
2022-11-15 16:42:10 +0300

    Добавили решение задачи №6

---
+++ task6_Leonova/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % 2 == 0)
+{
+    Console.WriteLine("Число является четным");
+
+}
+else 
+{
+    Console.WriteLine("Число является нечетным");
+}
