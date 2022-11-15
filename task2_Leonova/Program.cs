96b81ec7732237651907ed4a3f2695407cc64f24
marina.leonova <leona_21@mail.ru>
2022-11-15 16:27:01 +0300

    Добавили домашнюю работу задача 2

---
+++ task2_Leonova/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+if (number2 > max)
+{
+    Console.WriteLine("Число 2 является максимальным");
+
+}
+else 
+{
+    Console.WriteLine("Число 1 является максимальным");
+}
